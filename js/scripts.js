e3ddb88a04ac47036949438a4469d8bf349da0a8
samantha <samantha.s.var@gmail.com>
2018-05-06 21:17:04 -0700

    added title, input & submit box, ul, push todoArray, append todoList

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+// Access the input's value and the uls using DOM methods and store them in variables.
+// When the submit button is clicked get the user input and add it to an array.
+// Use whatever visual framework (or plain CSS styling) you like to make your page look good. Treat this like a product that you are building for a client. How would they want it to look and function?
+
+const todoList = document.querySelector('ul#todoList');
+
+let todoArray = [];
+
+document.forms.addItem.submitBtn.addEventListener('click', e => {
+  e.preventDefault();
+  todoArray.push(document.forms.addItem.newItem.value);
+  console.log(todoArray);
+  let item =  document.createElement('li');
+    item.textContent = document.forms.addItem.newItem.value;
+  todoList.appendChild(item);
+});
