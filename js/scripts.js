c11a63b628ddf709a29b42a81634df6e555598b4
samantha <samantha.s.var@gmail.com>
2018-05-06 22:40:30 -0700

    added list titles. prevented entering of blank item. cleared input field after submit button pushed.

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,18 @@
 // Use whatever visual framework (or plain CSS styling) you like to make your page look good. Treat this like a product that you are building for a client. How would they want it to look and function?
 
 const todoList = document.querySelector('ul#todoList');
+const form = document.getElementById('addItemForm')
 
 let todoArray = [];
 
 document.forms.addItem.submitBtn.addEventListener('click', e => {
   e.preventDefault();
-  todoArray.push(document.forms.addItem.newItem.value);
-  console.log(todoArray);
-  let item =  document.createElement('li');
-    item.textContent = document.forms.addItem.newItem.value;
-  todoList.appendChild(item);
+  if (document.forms.addItem.newItem.value !== "") {
+    todoArray.push(document.forms.addItem.newItem.value);
+    console.log(todoArray);
+    let item =  document.createElement('li');
+      item.textContent = document.forms.addItem.newItem.value;
+    todoList.appendChild(item);
+    form.reset();
+  };
 });
