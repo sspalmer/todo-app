18d1662df6fd392421918ce07207592d1a99b88c
samantha <samantha.s.var@gmail.com>
2018-05-12 18:09:40 -0700

    got check box to show

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,32 @@
 // When the submit button is clicked get the user input and add it to an array.
 // Use whatever visual framework (or plain CSS styling) you like to make your page look good. Treat this like a product that you are building for a client. How would they want it to look and function?
 
-const todoList = document.querySelector('ul#todoList');
 const form = document.getElementById('addItemForm')
+const todoList = document.querySelector('ul#todoList');
 
 let todoArray = [];
+let doneArray = [];
 
-document.forms.addItem.submitBtn.addEventListener('click', e => {
+
+document.forms.addItemForm.submitBtn.addEventListener('click', e => {
   e.preventDefault();
-  if (document.forms.addItem.newItem.value !== "") {
-    todoArray.push(document.forms.addItem.newItem.value);
-    console.log(todoArray);
-    let item =  document.createElement('li');
-      item.textContent = document.forms.addItem.newItem.value;
+
+  if (document.forms.addItemForm.newItem.value !== "") {
+    todoArray.push(document.forms.addItemForm.newItem.value);
+     console.log(todoArray);
+
+    let item =  document.createElement('form');
+      item.textContent = document.forms.addItemForm.newItem.value;
+
+    let checkbox = document.createElement('input');
+      checkbox.setAttribute('type', 'checkbox');
+      item.appendChild(checkbox);
+
+    let todoLabel = document.createElement('label');
+      
+
     todoList.appendChild(item);
     form.reset();
+
   };
 });
